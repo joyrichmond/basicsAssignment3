14d40c9a76ac79a91e4731696f1bc59b52274d5d
Joy <sjoy.richmond@gmail.com>
2019-03-01 17:47:26 -0600

    stuck onLogClicks behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .postFifth {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  toggleOn = false;
+  clickCount = 0;
+  clickLog = [];
+
+  onLogClicks(event) {
+    if(this.toggleOn == false) {
+      this.toggleOn = true;
+    } else {
+      this.toggleOn = false;
+    }
+    this.clickCount++;
+    this.clickLog.push(`Click number ${this.clickCount}`);
+  }
+
+  getColor() {
+    if(this.clickCount >= 5) {
+      return "blue";
+    }
+  }
 }
